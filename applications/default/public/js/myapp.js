b0810ff8090c51be41fc90079417e1772aa5b2d6
Pascal <contact@pmclogiciels.fr>
2023-09-22 23:28:15 +0200

    CHANGE: password is changed

--- applications/default/public/js/myapp.js
+++ applications/default/public/js/myapp.js
@@ -2,6 +2,6 @@ $(document).ready(function(){
 	/* Init login & password for the demo version */
 	$('#zdk-login-dialog').bind("zdklogindialogaftershow", function() {
 		$('#zdk-login-dialog input[name=login_name]').val('znetdk');
-		$('#zdk-login-dialog input[name=password]').val('demo');
+		$('#zdk-login-dialog input[name=password]').val('this1sDemoPwd');
 	});
 });
