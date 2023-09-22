406c461e8c40698acbe2be4d0f0e112f73cbb471
Pascal <contact@pmclogiciels.fr>
2023-09-22 23:30:29 +0200

    CHANGE: custom page layout, icons displayed for menu items

--- applications/default/public/js/menu.js
+++ applications/default/public/js/menu.js
@@ -1,14 +1,18 @@
-( function( $ ) {
-$( document ).ready(function() {
-$('#zdk-custom-menu').prepend('<div id="menu-button">Menu</div>');
-	$('#zdk-custom-menu #menu-button').on('click', function(){
-		var menu = $(this).next('ul');
-		if (menu.hasClass('open')) {
-			menu.removeClass('open');
-		}
-		else {
-			menu.addClass('open');
-		}
-	});
-});
-} )( jQuery );
+(function($) {
+    $(document).ready(function() {
+        $('#zdk-custom-menu').prepend('<div id="menu-button">Menu</div>');
+        $('#zdk-custom-menu #menu-button').on('click', function(){
+            var menu = $(this).next('ul');
+            if (menu.hasClass('open')) {
+                menu.removeClass('open');
+            } else {
+                menu.addClass('open');
+            }
+        });
+        $('#zdk-custom-menu a').each(function(){
+            var icon = $(this).data('icon'),
+                text = $(this).text();
+            $(this).html('<i class="fa fa-fw fa-lg ' + icon + '"></i>  ' + text);
+        });
+    });
+})(jQuery);
